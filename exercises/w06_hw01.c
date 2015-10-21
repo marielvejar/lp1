48028c062d47f9b9113620c2a524f9b4dda6baa8
marielvejar <marielvejar@users.noreply.github.com>
2015-10-21 00:42:17 -0300

    Create w06_hw01.c

---
+++ exercises/w06_hw01.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+  int hileras, i,j, espacio;
+  printf("Escribe un numero de hileras\n");
+  scanf("%d",&hileras);
+  for(i=hileras;i>=1;--i){
+    for(espacio=0;espacio<hileras-i;++espacio)
+    printf("*");
+    for(j=i;j<=2*i-1;++j)
+    printf(" ");
+    for(j=0;j<i-1;++j)
+    printf(" ");
+    printf("\n");
+  }
+  return 0;
+}
